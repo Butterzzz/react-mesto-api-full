3993043ec210bd9450251e0e52e8045df3191734
Grinchenko Sergei <butters.blaga@gmail.com>
2022-11-06 01:28:47 +0900

    feat: add requests & errors logger

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const { celebrate, Joi, errors } = require('celebrate');
 const { errorsHandler } = require('./middlewares/errorsHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -23,6 +24,7 @@ const limiter = rateLimit({
 });
 
 app.use(express.json());
+app.use(requestLogger); // логгер запросов
 
 // для защиты приложения от веб-уязвимостей путем соответствующей настройки заголовков HTTP
 app.use(helmet());
@@ -52,13 +54,9 @@ app.use('/cards', cardsRouter);
 app.use('/*', () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
-
-// обработчик ошибок celebrate
-app.use(errors());
-
-// централизованная обработка ошибок
-app.use(errorsHandler);
-
+app.use(errorLogger); // логгер ошибок
+app.use(errors()); // обработчик ошибок celebrate
+app.use(errorsHandler); // централизованная обработка ошибок
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
